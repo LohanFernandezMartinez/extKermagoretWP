40c148a27c0448de654e8f111349d2b901ffe2e9
Lohan Fernandez Martinez <lohanfernandezmartinez@gmail.com>
2023-03-16 10:11:50 +0000

    Formulaire PHP

--- public/wp-content/plugins/VeepIAPage/includes/vpia-first-acp-page.php
+++ public/wp-content/plugins/VeepIAPage/includes/vpia-first-acp-page.php
@@ -1,7 +1,30 @@
+<?php
+$foot='';
+$basket='';
+
+// On vérifie si le formulaire a été soumis
+if (isset($_POST['foot']) && isset($_POST['basket'])) {
+  $foot = 'checked';
+  $basket = 'checked';
+  update_user_meta(get_current_user_id(), 'foot', $foot);
+  update_user_meta(get_current_user_id(), 'basket', $basket);
+  echo get_current_user_id();
+}
+?>
+
 <div class="wrap">
   <h1>Veep IA</h1>
   <p>Voici une extension permettant de créer une page web et son contenu</p>
-  <form action="" method="post">
-    
-  </form>
+  <form method="post">
+  <p>Quels sont vos loisirs préférés ?</p>
+  <input type="checkbox" name="foot" <?php echo esc_attr($foot); ?>>
+  <label>Foot</label>
+  <input type="checkbox" name="basket" <?php echo esc_attr($basket); ?>>
+  <label>Basket</label>
+  <!--<input type="checkbox" id="lecture" name="loisirs[]" value="lecture">
+  <label for="lecture">Lecture</label>
+  <input type="checkbox" id="musique" name="loisirs[]" value="musique">
+  <label for="musique">Musique</label>-->
+  <input type="submit" name="submit" value="Envoyer">
+</form>
 </div>
